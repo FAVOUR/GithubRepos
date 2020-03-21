1c2512d54891bbf1e1b74bc777664f595a3e8d8d
FAVOUR <truelyfavoured@gmail.com>
2020-03-21 22:07:11 +0100

    Setting up the basics

---
+++ app/src/main/java/com/example/githubrepos/data/GithubLocalCache.kt
@@ -0,0 +1,9 @@
+package com.example.githubrepos.data
+
+import com.example.githubrepos.db.RepoDao
+import java.util.concurrent.Executor
+
+class GithubLocalCache(repoDao:RepoDao,ioExecutor: Executor) {
+
+
+}
